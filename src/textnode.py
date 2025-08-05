3466f1bc7cb96d3434a3875b21d4a13ca2a6f306
Dr3iundZwanzig <zaude.sven@gmail.com>
2025-08-05 15:30:40 +0000

    added text_node_to_html_node function

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from htmlnode import HTMLnode, LeafNode
 
 class TextType(Enum):
+    __order__= "TEXT BOLD ITALIC CODE LINK IMAGE"
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
@@ -21,3 +23,21 @@ class TextNode():
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
         
+def text_node_to_html_node(text_node):
+    if text_node.text_type not in TextType:
+        raise ValueError(f"unknown text type: {text_node.text_type}")
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text,{"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+        case _:
+            return "no case found"
