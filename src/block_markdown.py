caed509c2c05f857a7e3ca6b724fdf7671e34240
Dr3iundZwanzig <zaude.sven@gmail.com>
2025-08-11 15:02:05 +0000

    added block_to_block_type function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,15 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    __order__= "PARAGRAPH HEADING CODE QUOTE UNORDERED_LIST ORDERED_LIST"
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
 def markdown_to_blocks(markdown):
     sections = markdown.split("\n\n")
     final_list = []
@@ -7,3 +19,31 @@ def markdown_to_blocks(markdown):
             continue
         final_list.append(section)
     return final_list
+
+def block_to_block_type(text):
+    split_newline = text.split("\n")
+    quote = True
+    unordered_list = True
+    ordered_list = True
+    i = 1
+    for line in split_newline:
+        if not line.startswith(">"):
+            quote = False
+        if not line.startswith("- "):
+            unordered_list = False
+        if not line.startswith(f"{i}. "):
+            ordered_list = False
+        i += 1
+    if quote == True:
+        return BlockType.QUOTE
+    if text.startswith("```") and text.endswith("```"):
+        return BlockType.CODE
+    if unordered_list == True:
+        return BlockType.UNORDERED_LIST
+    if ordered_list == True:
+        return BlockType.ORDERED_LIST
+    matches = re.findall(r"^#{1,6} ", text)
+    if len(matches) != 0:
+        return BlockType.HEADING
+    return BlockType.PARAGRAPH
+    
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestExtractMarkdown(unittest.TestCase):
@@ -46,6 +46,53 @@ This is the same paragraph on a new line
             ],
         )
 
+class TestBlockToBlockType(unittest.TestCase):
+    def test_block_type_heading(self):
+        text1 = "### this is a heading"
+        text2 = "##### also heading"
+        blocktype1 = block_to_block_type(text1)
+        blocktype2 = block_to_block_type(text2)
+
+        self.assertEqual(BlockType.HEADING, blocktype1)
+        self.assertEqual(BlockType.HEADING, blocktype2)
+
+    def test_block_type_code(self):
+        text = "``` this is code ```"
+        blocktype = block_to_block_type(text)
+        
+        self.assertEqual(BlockType.CODE, blocktype)
+
+    def test_block_type_quote(self):
+        text = "> hund\n> katze\n> vogel"
+        blocktype = block_to_block_type(text)
+        
+        self.assertEqual(BlockType.QUOTE, blocktype)
+    
+    def test_block_type_unordered_list(self):
+        text = "- hund\n- katze\n- vogel"
+        blocktype = block_to_block_type(text)
+        
+        self.assertEqual(BlockType.UNORDERED_LIST, blocktype)
+
+    def test_block_type_ordered_list(self):
+        text = "1. hund\n2. katze\n3. vogel"
+        blocktype = block_to_block_type(text)
+        
+        self.assertEqual(BlockType.ORDERED_LIST, blocktype)
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
